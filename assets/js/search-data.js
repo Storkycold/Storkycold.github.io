f34ca4aef062c6b27887ec91158b2275a24f0d36
Storkycold <39956521+Storkycold@users.noreply.github.com>
2025-06-11 06:51:23 +0000

    Deploying to gh-pages from @ Storkycold/Storkycold.github.io@5510ac1e5b131b028d53e1493f3adc1a76c430b0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -55,9 +55,9 @@ ninja.data = [{
           description: "Data Youth Campus @Korea University",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-aitia-series-monitoring-system",
+          title: 'Aitia-Series Monitoring System',
+          description: "internship project",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
