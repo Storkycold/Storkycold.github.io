a27a42c25a9f2c3691ab8a3975339cf212f54e77
Storkycold <39956521+Storkycold@users.noreply.github.com>
2025-06-15 08:04:44 +0000

    Deploying to gh-pages from @ Storkycold/Storkycold.github.io@05fac5c3f8534d95df702706a3d7577e146fe0de 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -53,9 +53,9 @@ ninja.data = [{
           description: "internship project",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-data-serialization-system",
+          title: 'Data Serialization System',
+          description: "Configured Avro schema",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-nq-dpr-hard-benchmark",
